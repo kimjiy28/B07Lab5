5d0331fb0200969bb1e7ab8f97e50331db3238c4
Jiyeon <04jemma@gmail.com>
2024-10-23 21:41:25 -0400

    added isSquare method

--- Rhombus.java
+++ Rhombus.java
@@ -11,14 +11,16 @@ public class Rhombus {
 	}
 
 	public double side() {
-		return Math.sqrt(Math.pow(diagonalA,2) + Math.pow(diagonalB)) / 2.0; 
-	}
-
-	public double area() { 
-		return diagonalA * diagonalB / 2.0;
+		return Math.sqrt(Math.pow(diagonalA,2) + Math.pow(diagonalB, 2)) / 2.0; 
 	}
 
 	public double perimeter() {
 		return 4 * this.side();
+	}
+
+	public boolean isSquare() {
+		if (diagonalA == diagonalB) 
+			return true;
+		return false; 
 	}
 }
